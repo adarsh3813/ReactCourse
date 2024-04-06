cbdf22da25bb68eb1a639e425daee0e0ad44a885
adarsh3813 <adarshdubey444@gmail.com>
2024-04-06 21:59:43 +0530

    Fundamentals covered

--- IgnitingReactApp/index.js
+++ IgnitingReactApp/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// React
 const header1 = React.createElement(
   "h1",
   {
@@ -9,30 +11,40 @@ const header1 = React.createElement(
     style: {
       color: "red",
     },
+    key: "h1",
   },
   "Namaste Everyone"
 );
-const header2 = React.createElement(
-  "h2",
-  {
-    id: "header2",
-    style: {
-      color: "blue",
-      backgroundcolor: "green",
-    },
-  },
-  "Welcome some bakchodu in life"
+// root.render(container);
+
+// JSX
+const header2 = <h1 id="header2">Namaste JSX!</h1>;
+
+// Functional Component
+const HeaderComponent = () => {
+  return (
+    <div>
+      <h1>Namaste Functional Component</h1>
+      <h2>h2 tag in Component</h2>
+    </div>
+  );
+};
+
+const normalJsVariable = "Normal Js Variable";
+
+const Title = () => <h2>Sub heading functional Component</h2>;
+
+const HeaderComponent2 = () => (
+  <div>
+    {header2}
+    <HeaderComponent />
+    <h2>h2 Tag</h2>
+    <h3>h3 tag</h3>
+    {Title()}
+    {normalJsVariable}
+    {25 + 25}
+    {"Write any js code in curly braces"}
+  </div>
 );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-    style: {
-      backgroundColor: "yellow",
-    },
-  },
-  [header1, header2]
-);
-
-root.render(container);
+root.render(<HeaderComponent2 />);
